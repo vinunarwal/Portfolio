d97be3dcbb4f471e77c76dd5f642835431bc0bf3
vinunarwal <vinunarwal3@gmail.com>
2024-07-16 17:50:03 +0530

    ..

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
             className="absolute w-28 h-28 md:w-28 md:h-28 lg:w-40 lg:h-40 bg-purple-500 opacity-20 rounded-full"
             animate={{ scale: [1, 1.2, 1], opacity: [0.5, 0.2, 0.5] }}
             transition={{ duration: 9, repeat: Infinity, ease: 'easeInOut' }}
-            style={{ top: '45%', right: '10%' }}
+            style={{ top: '40%', right: '15%' }}
           >
             <img src={purpleParticle} alt="Purple Particle" className="w-full h-full rounded-full opacity-30" />
           </motion.div>
@@ -33,7 +33,7 @@ const Hero = () => {
             className="absolute w-32 h-32 md:w-32 md:h-32 lg:w-48 lg:h-48 bg-green-400 opacity-20 rounded-full"
             animate={{ scale: [1, 1.3, 1], opacity: [0.5, 0.2, 0.5] }}
             transition={{ duration: 12, repeat: Infinity, ease: 'easeInOut' }}
-            style={{ bottom: '7%', left: '20%' }}
+            style={{ bottom: '7%', left: '45%' }}
           >
             <img src={greenParticle} alt="Green Particle" className="w-full h-full rounded-full opacity-30" />
           </motion.div>
