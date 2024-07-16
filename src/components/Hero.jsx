0459fedb313e298ab4edc2a5e41fa5fb3ed04602
vinunarwal <vinunarwal3@gmail.com>
2024-07-16 17:24:13 +0530

    ..

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,19 +21,19 @@ const Hero = () => {
           </motion.div>
           {/* Purple Particle */}
           <motion.div
-            className="absolute w-20 h-20 md:w-28 md:h-28 lg:w-40 lg:h-40 bg-purple-500 opacity-20 rounded-full"
+            className="absolute w-28 h-28 md:w-28 md:h-28 lg:w-40 lg:h-40 bg-purple-500 opacity-20 rounded-full"
             animate={{ scale: [1, 1.2, 1], opacity: [0.5, 0.2, 0.5] }}
             transition={{ duration: 9, repeat: Infinity, ease: 'easeInOut' }}
-            style={{ top: '50%', right: '27%' }}
+            style={{ top: '45%', right: '10%' }}
           >
             <img src={purpleParticle} alt="Purple Particle" className="w-full h-full rounded-full opacity-30" />
           </motion.div>
           {/* Green Particle */}
           <motion.div
-            className="absolute w-24 h-24 md:w-32 md:h-32 lg:w-48 lg:h-48 bg-green-400 opacity-20 rounded-full"
+            className="absolute w-32 h-32 md:w-32 md:h-32 lg:w-48 lg:h-48 bg-green-400 opacity-20 rounded-full"
             animate={{ scale: [1, 1.3, 1], opacity: [0.5, 0.2, 0.5] }}
             transition={{ duration: 12, repeat: Infinity, ease: 'easeInOut' }}
-            style={{ bottom: '10%', left: '40%' }}
+            style={{ bottom: '7%', left: '20%' }}
           >
             <img src={greenParticle} alt="Green Particle" className="w-full h-full rounded-full opacity-30" />
           </motion.div>
@@ -48,7 +48,8 @@ const Hero = () => {
         className="relative z-10 text-center"
       >
         <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold leading-tight">Hey, I'm Vinu</h1>
-        <p className="mt-2 md:mt-4 text-lg md:text-2xl mb-7">A passionate Web Developer</p>
+        <p className="mt-2 md:mt-4 text-lg md:text-2xl mb-2">A passionate Web Developer</p>
+        <p className="text-lg md:text-2xl mb-7">Building Bridges Between Vision and User Interaction</p>
         <motion.a
           href="#projects"
           whileHover={{ scale: 1.1 }}
